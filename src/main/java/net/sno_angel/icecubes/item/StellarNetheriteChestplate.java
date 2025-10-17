be2ca6e687107d03c5eff656e2b186212580093f
kati <wkf@amhran.net>
2025-10-17 15:07:57 -0400

    10/17/2025 15:07 - Stellar

---
+++ src/main/java/net/sno_angel/icecubes/item/StellarNetheriteChestplate.java
@@ -0,0 +1,9 @@
+package net.sno_angel.icecubes.item;
+
+import net.minecraft.item.Item;
+
+public class StellarNetheriteChestplate extends Item {
+    public StellarNetheriteChestplate(Settings settings) {
+        super(settings);
+    }
+}
