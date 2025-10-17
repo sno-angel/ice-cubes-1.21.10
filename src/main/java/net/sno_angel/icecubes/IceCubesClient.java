fba9070ff232a4f15076115a706772b3d4a52218
sno_angel <wkf@amhran.net>
2025-10-16 21:53:01 -0400

    10/16/2026 9:52 - Initial Commit

---
+++ src/main/java/net/sno_angel/icecubes/IceCubesClient.java
@@ -0,0 +1,10 @@
+package net.sno_angel.icecubes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IceCubesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/sno_angel/icecubes/IceCubesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.sno_angel.icecubes;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class IceCubesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
