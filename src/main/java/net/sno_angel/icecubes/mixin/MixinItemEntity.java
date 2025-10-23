c1a801400946078ee4bc88ad0aceb32a8265b1b0
sno_angel <wkf@amhran.net>
2025-10-23 00:07:59 -0400

    10/23/2025 00:08 - Mixin (also made a minor change to AP shard texture

---
+++ src/main/java/net/sno_angel/icecubes/mixin/MixinItemEntity.java
@@ -0,0 +1,34 @@
+package net.sno_angel.icecubes.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.Ownable;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.sno_angel.icecubes.IceCubes;
+import net.sno_angel.icecubes.util.ModItemTagProvider;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemEntity.class)
+public abstract class MixinItemEntity extends Entity implements Ownable {
+    public MixinItemEntity(EntityType<? extends ItemEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Shadow
+    public abstract ItemStack getStack();
+
+    @Inject(at = @At("TAIL"), method = "<init>(Lnet/minecraft/entity/EntityType;Lnet/minecraft/world/World;)V")
+    private void ItemEntity(EntityType<? extends ItemEntity> entityType, World world, CallbackInfo ci) {
+        if(this.getStack().isIn(ModItemTagProvider.CHORAFIL_ITEMS)) {
+            this.setNoGravity(true);
+            IceCubes.LOGGER.info("gravity");
+        }
+    }
+
+}
---
+++ src/main/java/net/sno_angel/icecubes/util/ModItemTagProvider.java
@@ -0,0 +1,48 @@
+package net.sno_angel.icecubes.util;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.sno_angel.icecubes.IceCubes;
+import net.sno_angel.icecubes.item.ModItems;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
+
+    public static final TagKey<Item> CHORAFIL_ITEMS = TagKey.of(RegistryKeys.ITEM, Identifier.of(IceCubes.MOD_ID, "chorafil_items"));
+    public static final TagKey<Item> AGED_PRISMARINE_ITEMS = TagKey.of(RegistryKeys.ITEM, Identifier.of(IceCubes.MOD_ID, "aged_prismarine_items"));
+
+    public ModItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        valueLookupBuilder(CHORAFIL_ITEMS)
+                .add(ModItems.RAW_CHORAFIL)
+                .add(ModItems.CRYSTALLIZED_CHORAFIL)
+                .add(ModItems.CHORAFIL_HELMET)
+                .add(ModItems.CHORAFIL_CHESTPLATE)
+                .add(ModItems.CHORAFIL_LEGGINGS)
+                .add(ModItems.CHORAFIL_BOOTS)
+                .add(ModItems.WINGED_CHORAFIL_CHESTPLATE)
+                .setReplace(true);
+
+        valueLookupBuilder(AGED_PRISMARINE_ITEMS)
+                .add(ModItems.AGED_PRISMARINE_SHARD)
+                .add(ModItems.AGED_PRISMARINE_PLATE)
+                .add(ModItems.AGED_PRISMARINE_HELMET)
+                .add(ModItems.AGED_PRISMARINE_CHESTPLATE)
+                .add(ModItems.AGED_PRISMARINE_LEGGINGS)
+                .add(ModItems.AGED_PRISMARINE_BOOTS)
+                .add(ModItems.OCEANIC_AGED_PRISMARINE_CHESTPLATE)
+                .setReplace(true);
+
+        IceCubes.LOGGER.debug("configure");
+    }
+}
