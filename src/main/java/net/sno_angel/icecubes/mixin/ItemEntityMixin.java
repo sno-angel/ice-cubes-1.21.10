a681f39c3358b8c9dab780bbbe48256ed8f88c9a
sno_angel <wkf@amhran.net>
2025-10-23 16:55:46 -0400

    10/23/2025 16:55 - Reverted ItemEntityMixin to a working state

--- src/main/java/net/sno_angel/icecubes/mixin/ItemEntityMixin.java
+++ src/main/java/net/sno_angel/icecubes/mixin/ItemEntityMixin.java
@@ -15,8 +15,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemEntity.class)
-public abstract class MixinItemEntity extends Entity implements Ownable {
-    public MixinItemEntity(EntityType<? extends ItemEntity> entityType, World world) {
+public abstract class ItemEntityMixin extends Entity implements Ownable {
+    public ItemEntityMixin(EntityType<? extends ItemEntity> entityType, World world) {
         super(entityType, world);
     }
 
@@ -25,6 +25,7 @@ public abstract class MixinItemEntity extends Entity implements Ownable {
 
     @Inject(at = @At("TAIL"), method = "<init>(Lnet/minecraft/entity/EntityType;Lnet/minecraft/world/World;)V")
     private void ItemEntity(EntityType<? extends ItemEntity> entityType, World world, CallbackInfo ci) {
+        IceCubes.LOGGER.info("test");
         if(this.getStack().isIn(ModItemTagProvider.CHORAFIL_ITEMS)) {
             this.setNoGravity(true);
             IceCubes.LOGGER.info("gravity");
