bd16c276d89d502ce93ea8f2f8d91eb9d1fbfea0
kati <wkf@amhran.net>
2025-10-23 15:55:18 -0400

    10/22/2025 21:15 - Almost fully implemented and textured Aged Prismarine plates and shards, changed upgraded chestplate recipes to smithing

--- src/main/java/net/sno_angel/icecubes/IceCubesDataGenerator.java
+++ src/main/java/net/sno_angel/icecubes/IceCubesDataGenerator.java
@@ -2,12 +2,10 @@ package net.sno_angel.icecubes;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import net.sno_angel.icecubes.util.ModItemTagProvider;
 
 public class IceCubesDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-        var pack = fabricDataGenerator.createPack();
-        pack.addProvider(ModItemTagProvider::new);
+
 	}
 }
--- src/main/java/net/sno_angel/icecubes/mixin/ExampleMixin.java
+++ src/main/java/net/sno_angel/icecubes/mixin/ExampleMixin.java
@@ -1,6 +1,7 @@
 package net.sno_angel.icecubes.mixin;
 
 import net.minecraft.server.MinecraftServer;
+import net.sno_angel.icecubes.IceCubes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,6 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "loadWorld")
 	private void init(CallbackInfo info) {
+        IceCubes.LOGGER.debug("Mixins running!");
 		// This code is injected into the start of MinecraftServer.loadWorld()V
 	}
 }
